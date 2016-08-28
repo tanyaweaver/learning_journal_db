6531f4e493cd4a26086fc5099dea532d52a8d50f
tanyaweaver <tanya83@uw.edu>
2016-08-27 20:19:56 -0700

    chmod u+x run

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # learning_journal_db
+## Python web application implemented in Pyramid with sqlite.
+* 
+* test covearge py2/py3 79%
--- learning_journal_db/routes.py
+++ learning_journal_db/routes.py
@@ -3,5 +3,4 @@ def includeme(config):
     config.add_route('lists', '/')
     config.add_route('create', '/journal/new-entry')
     config.add_route('detail', r'/journal/{id:\d+}')
-    config.add_route('update', r'/journal/{id:\d+}/edit-entry')
-   
+    config.add_route('update', r'/journal/{id:\d+}/edit-entry') 
